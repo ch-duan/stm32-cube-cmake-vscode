667db3c96ffb3d61444e9d546a0ad352903a19ba
ch <52442848+ch-duan@users.noreply.github.com>
2024-08-09 18:15:51 +0800

    update

--- templates/cmake/gcc-arm-none-eabi.cmake
+++ templates/cmake/gcc-arm-none-eabi.cmake
@@ -8,17 +8,21 @@ string(REPLACE "\\" "/" STM_PATH ${STM_TOOLCHAIN})
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
 set(TOOLCHAIN_PREFIX                ${STM_PATH}/bin/arm-none-eabi-)
-set(FLAGS                           "-fdata-sections -ffunction-sections --specs=nano.specs -fstack-usage")
-set(CPP_FLAGS                       "${FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(FLAGS                           "-ffunction-sections -fdata-sections -fstack-usage -fcyclomatic-complexity")
+set(CPP_FLAGS                       "${FLAGS} -fno-exceptions -fno-rtti -fno-use-cxa-atexit")
 
 set(CMAKE_C_FLAGS                   ${FLAGS})
 set(CMAKE_CXX_FLAGS                 ${CPP_FLAGS})
 
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc.exe)
-set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++.exe)
-set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy.exe)
-set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size.exe)
+set(CMAKE_C_COMPILER   "${TOOLCHAIN_PREFIX}gcc.exe")
+set(CMAKE_ASM_COMPILER "${TOOLCHAIN_PREFIX}gcc.exe")
+set(CMAKE_CXX_COMPILER "${TOOLCHAIN_PREFIX}g++.exe")
+set(CMAKE_AR           "${TOOLCHAIN_PREFIX}ar.exe")
+set(CMAKE_LINKER       "{TOOLCHAIN_PREFIX}ld.exe")
+set(CMAKE_OBJCOPY      "${TOOLCHAIN_PREFIX}objcopy.exe")
+set(CMAKE_RANLIB       "${TOOLCHAIN_PREFIX}ranlib.exe")
+set(CMAKE_SIZE         "${TOOLCHAIN_PREFIX}size.exe")
+set(CMAKE_STRIP        "${TOOLCHAIN_PREFIX}ld.exe")
 
 set(CMAKE_EXECUTABLE_SUFFIX_ASM     ".elf")
 set(CMAKE_EXECUTABLE_SUFFIX_C       ".elf")
@@ -27,5 +31,5 @@ set(CMAKE_EXECUTABLE_SUFFIX_CXX     ".elf")
 set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
 
-# INCLUDE_DIRECTORIES(${STM_TOOL_PATH}/include)
+INCLUDE_DIRECTORIES(${STM_TOOL_PATH}/include)
 # INCLUDE_DIRECTORIES(${STM_TOOL_PATH}/include/c++/11.3.1)
